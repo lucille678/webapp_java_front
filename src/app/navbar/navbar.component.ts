bdaac3a72a5018514f253dcbe92dc21289c3ca1e
chiaracarlino <chiara.carlino@epfedu.fr>
2025-10-06 17:03:01 +0200

    Début du front - home page et navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,7 +10,7 @@ export class NavbarComponent {
   links: Link[] = []
 
   constructor() {
-    this.links.push({ name: "Étudiants", href: "etudiants" })
-    this.links.push({ name: "Filières", href: "filieres" })
+    this.links.push({ name: "Mes Portfolios", href: "creation" })
+    this.links.push({ name: "Mon audience", href: "statistics" })
   }
 }

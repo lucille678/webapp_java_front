1d81f75b704b9f861e4f97bafe796d22cce713e5
chiaracarlino <chiara.carlino@epfedu.fr>
2025-10-13 18:12:28 +0200

    Début du formulaire de template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import { HomeComponent } from "./home/home.component"
-import {CreationComponent} from "./creation/creation.component";
 import {StatisticsComponent} from "./statistics/statistics.component";
 import { RegisterComponent } from "./register/register.component";
 import { AuthGuard } from './services/auth.guard';
@@ -13,7 +12,6 @@ import { TemplateEditorComponent } from './creation/template-editor/template-edi
 
 const routes: Routes = [
   { path: "", component: HomeComponent},
-  { path: "creation", component: CreationComponent},
   { path: "statistics", component: StatisticsComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
--- src/app/creation/template-editor/template-editor.component.ts
+++ src/app/creation/template-editor/template-editor.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-template-editor',
   templateUrl: './template-editor.component.html',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule,
   ],
   styleUrls: ['./template-editor.component.scss']
 })
@@ -32,13 +34,34 @@ export class TemplateEditorComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // Load config comme avant
     this.templateName = this.route.snapshot.paramMap.get('templateName');
     if (this.templateName) {
-      this.http.get(`assets/templates/${this.templateName}/config.json`).subscribe(data => {
+      this.http.get(`assets/templates/${this.templateName}/config.json`).subscribe((data: any) => {
         this.config = data;
+        // Initialiser formData
+        this.config.sections.forEach((section: any) => {
+          if (section.repeatable) {
+            this.formData[section.name] = [];
+          } else {
+            this.formData[section.name] = {};
+          }
+        });
       });
     }
   }
+
+  addItem(sectionName: string) {
+    const sectionConfig = this.config.sections.find((s: any) => s.name === sectionName);
+    const newItem: any = {};
+    sectionConfig.fields.forEach((f: any) => newItem[f.name] = '');
+    this.formData[sectionName].push(newItem);
+  }
+
+  removeItem(sectionName: string, index: number) {
+    this.formData[sectionName].splice(index, 1);
+  }
+
 
   onSubmit() {
     console.log(this.formData);

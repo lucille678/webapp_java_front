d8a77e3cd1d861e1c3fc23c3c21baca75b2881b7
chiaracarlino <chiara.carlino@epfedu.fr>
2025-10-18 18:43:08 +0200

    templates modern et creative

---
+++ src/app/templates/creative/creative.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'creative',
+  standalone: true,
+  imports: [],
+  templateUrl: './creative.component.html',
+  styleUrl: './creative.component.scss'
+})
+export class CreativeComponent {
+
+}
